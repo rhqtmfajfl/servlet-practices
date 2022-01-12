f9348c28c0a74927b574bde7fa7be8f34db55b81
polar <polar@polaris402>
2022-01-12 21:05:04 +0900

    servlet-test

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,30 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	      response.setContentType("text/html;charset=utf-8");
+	      PrintWriter pw = response.getWriter();
+	      
+	      pw.println("<h1>Hello World</h1>");
+	      pw.println("<h1>안녕하세요</h1>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
