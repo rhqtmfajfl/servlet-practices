4cec789bbb1229c229811067c6b97111f7ef26b7
polar <polar@polaris402>
2022-01-12 23:24:37 +0900

    servlet-practices test

---
+++ db/db.sql
@@ -0,0 +1,7 @@
+-- db 생성
+create database webdb;
+
+-- user 생성
+create user 'webdb'@'localhost' identified by 'webdb';
+
+-- 권한 부여
--- helloweb/src/main/java/helloweb/HelloServlet.java
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -8,22 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	      response.setContentType("text/html;charset=utf-8");
-	      PrintWriter pw = response.getWriter();
-	      
-	      pw.println("<h1>Hello World</h1>");
-	      pw.println("<h1>안녕하세요</h1>");
+		// 요청처리(request handle)
+		
+		// 응답(response)  
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		pw.println("<h1>Hello World</h1>");
+		pw.println("<h1>안녕하세요.</h1>");
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
